a6081e1fa28e8e6a31c4489966a4be2ed9f246b5
smcgee31 <solodesign31@gmail.com>
2017-03-19 19:00:47 -0600

    Complete HelloReact_02 with notes

--- HelloReact_02/public/app.jsx
+++ HelloReact_02/public/app.jsx
@@ -1,66 +1,79 @@
-var GreeterMessage = React.createClass({
-  render: function() {
+var GreeterMessage = React.createClass({                      // This component is a presentational component. It simply
+  render: function() {                                        // takes a couple of props passed to it from it's parent and
+    var name = this.props.name;                               // renders them in the JSX of the return.
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Some Header</h1>
-        <p>Some Header</p>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
       </div>
-    )
+    );
   }
 });
 
-var Greeter = React.createClass({
+var GreeterForm = React.createClass({                         // This is also a presentational component, but has a little bit 
+  onFormSubmit: function (e) {                                // more functionality. It takes a prop from a parent, checks it's
+    e.preventDefault();                                       // length (which just checks to see that it exists) and then clears
+                                                              // the input field. Then calls the function that exists in the parent
+    var name = this.refs.name.value;                          // 'onNewName' that takes one parameter. onNewName itself calls the
+    if (name.length> 0) {                                     // 'handleNewName' function that uses setState()
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
+  },
 
-  getDefaultProps: function () {
-    return {
-      name: 'React',
+  render: function () {                                       // Then renders the form with the functionality set up for it.
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input type='text' ref='name'/>
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var Greeter = React.createClass({                             // Classic 'container' component as it ONLY renders the children
+                                                              // and holds state. It also holds default props in case props are
+  getDefaultProps: function () {                              // not set elsewhere and you'd end up getting nothing where you
+    return {                                                  // thought you'd get something. (default props are valuable like
+      name: 'React',                                          // default values in variables in raw javascript.)
       message: 'Hey I\'m a default message'
     };
   },
 
   getInitialState: function () {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     }
   },
 
-  onButtonClick: function (e) {
-    e.preventDefault();
-
-    var name = this.refs.name.value;
-
-    this.refs.name.value = '';
-
-    if (typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleNewName: function (name) {                            // Always remember that setState() does not set the state like you 
+    this.setState({                                           // might think. It creates a pending state transition. Treat it like 
+        name: <name></name>                                   // it is immutable. "There is no guarantee of synchronous operation calls
+      });                                                     // to setState and calls may be batched for performance gains." per React docs.
     
   },
 
   render: function () { 
     var name = this.state.name;
+    var message = this.props.message;
 
     return (
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{this.props.message + '!!'}</p>
 
-        <GreeterMessage/>
+        <GreeterMessage name={name} message={message}/>
 
-        <form onSubmit={this.onButtonClick}>
-          <input type='text' ref='name'/>
-          <button>Set Name</button>
-        </form>
-
-        <GreeterForm/>
+        <GreeterForm onNewName={this.handleNewName}/>
 
       </div>
-    )
+    );
   }
 
-});
+}); 
 
 var firstName = 'Steve';
 
